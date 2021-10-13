f42dc0f901506a54248c794f1fad03bba17e4d05
XIONGJIAMAN <1957921021@qq.com>
2021-10-13 09:33:08 +0800

    update

---
+++ 09-快排.js
@@ -0,0 +1,32 @@
+/***
+ * 快排是最常见的排序方法之一，这次用 JS 手动实现一下。
+
+    快排是典型的“分而治之”，分治法的基本思想是：将原问题分解为若干个规模更小但结构与原问题相似的子问题。递归地解这些子问题，然后将这些子问题的解组合为原问题的解。
+
+    利用分治法可将快速排序的分为三步：
+
+    1. 在数据集之中，选择一个元素作为”基准”（pivot）。
+    2. 所有小于”基准”的元素，都移到”基准”的左边；所有大于”基准”的元素，都移到”基准”的右边。这个操作称为分区 (partition) 操作，分区操作结束后，基准元素所处的位置就是最终排序后它的位置。
+    3. 对”基准”左边和右边的两个子集，不断重复第一步和第二步，直到所有子集只剩下一个元素为止。
+ */
+function jsQuickSort(array) {
+    //递归出口 我们一旦用到递归一定要先想出口
+    if (array.length <= 1) {
+        return array;
+    }
+    const pivotIndex = Math.floor(array.length / 2);
+    const pivot = array.splice(pivotIndex, 1)[0];  //从数组中取出我们的"基准"元素
+    const left = [], right = [];
+    array.forEach(item => {
+        if (item < pivot) {  //left 存放比 pivot 小的元素
+            left.push(item); 
+        } else {  //right 存放大于或等于 pivot 的元素
+            right.push(item);
+        }
+    });
+    //至此，我们将数组分成了left和right两个部分
+    return jsQuickSort(left).concat(pivot, jsQuickSort(right));  //分而治之
+}
+
+const arr = [98, 42, 25, 54, 15, 3, 25, 72, 41, 10, 121];
+console.log(jsQuickSort(arr));  //输出：[ 3, 10, 15, 25, 25, 41, 42, 54, 72, 98, 121 ]
